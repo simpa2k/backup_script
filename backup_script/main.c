7fb7261502ef32422b05c1bba25c66881989030a
Simon Olofsson <s.olofsson@gmail.com>
2016-09-05 15:24:35 +0200

    Initial Commit

---
+++ backup_script/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  backup_script
+//
+//  Created by Simon Olofsson on 2016-09-05.
+//  Copyright © 2016 Simon Olofsson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
