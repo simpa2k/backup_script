8e64f39d0861bfe3b2baace24ce22616508dd3fa
Simon Olofsson <s.olofsson@gmail.com>
2016-09-11 13:27:44 +0200

    Threads now check stack for tasks to perform.

--- backup_script/main.c
+++ backup_script/main.c
@@ -7,57 +7,103 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <dirent.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <copyfile.h>
+#include <stdbool.h>
 #import <pthread.h>
 
-void *copy_thread(void *backup_info);
+#include "stack.h"
+
+#define CPUS 2
+#define NUM_THREADS CPUS + 1
+
+Stack *populate_stack();
+int threaded_backup(Stack *S, char *destination);
+void *copydir_thread(void *thread_args);
 int copy_file(const char *from, const char *to);
 int copydir(const char *dirname, const char *destination_root);
 int compare_dates(const char *file1, const char *file2);
 char *get_filename(const char *path);
 void concat_paths(char *output, long output_size, const char *base, const char *extension);
 
-struct backup_information {
-    char orig_path[1024];
+struct thread_args {
+    Stack *S;
     char destination[1024];
+    pthread_mutex_t lock;
 };
 
 int main(int argc, const char * argv[]) {
-    const char *paths[2];
-    pthread_t threads[2];
-    //pthread_t pth1, pth2;
     
-    paths[0] = "/Users/simpa2k/C/backup_script/backup_script/testdir";
-    paths[1] = "/Users/simpa2k/C/backup_script/backup_script/testdir2";
-    
+    Stack *S = populate_stack();
     char destination[] = "/Users/simpa2k/C/backup_script/backup_script/destination";
     
-    for(int i = 0; i < sizeof(paths) / sizeof(char *); i++) {
-        //copydir(paths[i], destination);
-        struct backup_information *info = malloc(sizeof(*info));
-        
-        strcpy(info->orig_path, paths[i]);
-        strcpy(info->destination, destination);
-    
-        pthread_create(&threads[i], NULL, copy_thread, info);
+    return threaded_backup(S, destination);
+}
 
+Stack *populate_stack() {
+    char *paths[3];
+    paths[0] = "/Users/simpa2k/C/backup_script/backup_script/testdir";
+    paths[1] = "/Users/simpa2k/C/backup_script/backup_script/testdir2";
+    paths[2] = "/Users/simpa2k/C/backup_script/backup_script/testdir3";
+    
+    Stack *S = createStack();
+    
+    for(int i = 0; i < sizeof(paths) / sizeof(char *); i++) {
+        push(S, paths[i]);
     }
     
-    for(int i = 0; i < sizeof(threads) / sizeof(pthread_t *); i++) {
+    return S;
+}
+
+int threaded_backup(Stack *S, char *destination) {
+    pthread_t threads[2];
+    pthread_mutex_t lock;
+    
+    if(pthread_mutex_init(&lock, NULL) != 0) {
+        printf("Mutex init failed\n");
+        return -1;
+    };
+    
+    for(int i = 0; i < NUM_THREADS && i < getSize(S); i++) {
+        struct thread_args *thr_args = malloc(sizeof(*thr_args));
+        
+        thr_args->S = S;
+        strcpy(thr_args->destination, destination);
+        thr_args->lock = lock;
+        
+        pthread_create(&threads[i], NULL, copydir_thread, thr_args);
+        
+    }
+    
+    for(int i = 0; i < NUM_THREADS; i++) {
         pthread_join(threads[i], NULL);
     }
     
+    pthread_mutex_destroy(&lock);
+
     return 0;
 }
 
-void *copy_thread(void *backup_info) {
-    struct backup_information *info = backup_info;
+void *copydir_thread(void *thread_args) {
+    struct thread_args *thr_args = thread_args;
     
-    copydir(info->orig_path, info->destination);
+    while(true) {
+        pthread_mutex_lock(&thr_args->lock);
+        char *path = pop(thr_args->S);
+        pthread_mutex_unlock(&thr_args->lock);
+        
+        if(path != NULL) {
+            copydir(path, thr_args->destination);
+        } else {
+            break;
+        }
+        
+    }
+    
     
     return NULL;
 }
